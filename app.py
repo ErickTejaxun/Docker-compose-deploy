78d432c77bb27e93951d5e5faaa64c3b8541802e
ErickTejaxun <erickteja@gmail.com>
2020-10-03 05:51:15 +0200

    Fix --Port of database server --Server update libraries

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
-import time
 import mysql.connector
 
 from flask import Flask
 app = Flask(__name__)
 
-contador = 0
+data = '-'
 
 
-
-def get_hit_count():
-    global contador
-    contador = contador +1 
-    return contador
 
 @app.route('/')
 def hello():
-    count = get_hit_count()
-    return 'Hello World! I have been seen {} times.\n'.format(count)
+    mydb = mysql.connector.connect(host='172.18.0.3',port='3306',user='root',password='root',database='sa_db',auth_plugin='mysql_native_password')    
+    mycursor = mydb.cursor()
+    mycursor.execute('SELECT * FROM usuario')
+    myresult = mycursor.fetchall()
+    data = ''
+    for x in myresult:
+        #print(x)
+        data = data + '\n\n' + str(x)
+        #print(mydb)
+    return 'Contenido de la base de datos {} \n'.format(data)
